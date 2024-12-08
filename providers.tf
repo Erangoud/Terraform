27f882259915d421ad57632608894f1192dcce8f
Erangoud <egoud83@gmail.com>
2024-12-08 14:06:53 +0530

    removed the keys

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
    region = "ap-south-1"
-    access_key = "AKIA3TD2SLYQDKECMQM6"
-    secret_key = "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"
+    access_key = "AKIA3TD2**************"
+    secret_key = "tGsjmOVjVPiJn9xUgyCC**************"
 }
